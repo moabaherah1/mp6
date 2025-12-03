92d40f14e231a549aec7179e41899f39813b392d
Mohammed Abaherah <abaherah@bu.edu>
2025-12-02 21:15:42 -0500

    fixing

--- app/about/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function AboutPage() {
-    return (
-        <div className="flex flex-col justify-cemter items-center bg-red-300 p-2">
-            <h1> I am a CS391 Student</h1>
-            <p>This is a project in the class</p>
-        </div>
-    )
-}
